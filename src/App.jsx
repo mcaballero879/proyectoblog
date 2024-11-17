35b15264ae8e2260acad81f5d0a4124e8a214caa
mcaballero879 <mcaballero879@gmail.com>
2024-11-17 12:00:04 -0300

    Segunda subida solo robot

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='imagen'>
+        <img src='https://static.wixstatic.com/media/7ac524_7cd5a0e1eef64998b13ecd7f46b4c6a9~mv2.png/v1/fill/w_160,h_131,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/7ac524_7cd5a0e1eef64998b13ecd7f46b4c6a9~mv2.png' alt='imagen robot'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
     </>
   )
 }
